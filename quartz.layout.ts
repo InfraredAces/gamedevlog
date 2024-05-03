f835ddb4d6d3e315a5fadfd676e54c2fa06f2cb9
Kevin Nguyen <nguyenkevin625@gmail.com>
2024-05-02 23:01:16 -0700

    Move files above folders in explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,7 +25,20 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        sortFn: (a, b) => {
+          if ((!a.file && !b.file) || (a.file && b.file)) {
+            return a.displayName.localeCompare(b.displayName)
+          }
+          if (a.file && !b.file) {
+            return -1
+          } else {
+            return 1
+          }
+        },
+      }),
+    ),
   ],
   right: [
     Component.Graph(),
@@ -42,7 +55,20 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        sortFn: (a, b) => {
+          if ((!a.file && !b.file) || (a.file && b.file)) {
+            return a.displayName.localeCompare(b.displayName)
+          }
+          if (a.file && !b.file) {
+            return -1
+          } else {
+            return 1
+          }
+        },
+      }),
+    ),
   ],
   right: [],
 }
