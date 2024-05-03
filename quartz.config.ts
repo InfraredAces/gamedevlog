46e84513aef54415c25b350d6f10cd98d3616f64
Kevin Nguyen <nguyenkevin625@gmail.com>
2024-05-03 12:55:44 +0000

    Change CrawlLinks settings

--- quartz.config.ts
+++ quartz.config.ts
@@ -92,7 +92,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "absolute", openLinksInNewTab: true }),
       Plugin.Description(),
     ],
     filters: [Plugin.RemoveDrafts()],
